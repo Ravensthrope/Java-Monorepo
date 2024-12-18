ae873484315b5268b642f3948ad384a434ac1c8f
Aafaq Sayed <sdaafaq@gmail.com>
2024-12-18 14:05:45 +0530

    initial commit

---
+++ client-gradle/src/main/java/com/example/client_gradle/ClientGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.client_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientGradleApplication.class, args);
+	}
+
+}
---
+++ client-gradle/src/test/java/com/example/client_gradle/ClientGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.client_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
